149b96c74bf2991b1656503ac11de5fe3e816a2b
Kerctq <islapnah@gmail.com>
2021-03-27 16:32:56 +0200

    ydkhfd

---
+++ reg-form/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container mt-4" >
+    <div class="row">
+      <div class="col-sm-4 mx-auto">
+    <form>
+
+      <div class="form-group">
+        <label for="name" class="form-label">Email address</label>
+        <input type="text" class="form-control" id="name">
+      </div>
+
+      <!-- По образцу выше сделать остальные поля -->
+
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+
+      <button type="button" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
